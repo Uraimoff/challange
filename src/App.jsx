4dcb35c20014fcae8cbb9b2c016764c72dff99bb
https://github.com/Uraimoff <uraimovibrohim5@gmail.com>
2022-12-06 13:37:41 +0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
-import {Routes, Route} from 'react-router-dom'
-import { navbar } from "./utils/navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+// import  navbar  from "./utils/navbar";
+import Home from "./pages/Home";
+import Question from "./pages/Question";
+import DailyRate from "./pages/DailyRate";
+import AllRate from "./pages/AllRate";
 
 function App() {
-
   return (
+    // <div>Hello</div>
     <Routes>
-      {
-        navbar.map((value)=>(
-          <Route path={value.path} element={value.element} />
-        ))
-      }
+      <Route path="/" element={<Navigate to={"/home"} />} />
+      <Route path="*" element={<Navigate to={"/home"} />} />
+      <Route path="/home" element={<Home />} /> 
+      <Route path="/auth" element={<Registration />} /> 
+      <Route path="/all" element={<AllRate />} /> 
+      <Route path="/daily" element={<DailyRate />} /> 
+      <Route path="/quest" element={<Question />} /> 
     </Routes>
   );
 }
--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -4,41 +4,41 @@ import Home from "../pages/Home";
 import Question from "../pages/Question";
 import Registration from "../pages/Registration";
 
-const navbar=[
-    {
-        element: <Home/>,
-        title: "Home",
-        path: "/home",
-        private: false,
-        hidden: false,
-    },
-    {
-        element: <AllRate/>,
-        title: "All rating",
-        path: "/all",
-        private: false,
-        hidden: false,
-    },
-    {
-        element: <DailyRate/>,
-        title: "Daily rating",
-        path: "/daily",
-        private: false,
-        hidden: false,
-    },
-    {
-        element: <Question/>,
-        title: "Question",
-        path: "/quest",
-        private: false,
-        hidden: false,
-    },
-    {
-        element: <Registration/>,
-        title: "Registration",
-        path: "/auth",
-        private: false,
-        hidden: false,
-    },
-]
-export {navbar}
+const navbar = [
+  {
+    element: <Home />,
+    title: "Home",
+    path: "/home",
+    private: false,
+    hidden: false,
+  },
+  {
+    element: <AllRate />,
+    title: "All rating",
+    path: "/all",
+    private: false,
+    hidden: false,
+  },
+  {
+    element: <DailyRate />,
+    title: "Daily rating",
+    path: "/daily",
+    private: false,
+    hidden: false,
+  },
+  {
+    element: <Question />,
+    title: "Question",
+    path: "/quest",
+    private: false,
+    hidden: false,
+  },
+  {
+    element: <Registration />,
+    title: "Registration",
+    path: "/auth",
+    private: false,
+    hidden: false,
+  },
+];
+export default navbar;
