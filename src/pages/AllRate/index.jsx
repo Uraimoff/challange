1df255c4cfec9da995609b89ac2a0e7c6dc70f06
https://github.com/Uraimoff <uraimovibrohim5@gmail.com>
2022-12-06 15:18:57 +0500

    table finished

--- src/pages/AllRate/index.jsx
+++ src/pages/AllRate/index.jsx
@@ -5,8 +5,12 @@ import './style.scss'
 const AllRate = () => {
   return (
     <>
-    <div className='wrapper'>
+    <div className='wrapper GlobaWrapper'>
+      <h2>15.12.00</h2>
+      <div className='table'>
+      <h3>All users score</h3>
     <Tables/>
+      </div>
     </div>
     </>
   )
