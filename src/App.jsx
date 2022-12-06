06baa40e76bbc65a8e47bbc097b130efbd62a1a1
Пользователь <suhrob.coder0604@gmail.com>
2022-12-06 13:37:28 +0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./App.css";
-import {Routes, Route} from 'react-router-dom'
-import { navbar } from "./utils/navbar";
+// import { Routes, Route } from "react-router-dom";
+import navbar from "./utils/navbar"
 
 function App() {
-
   return (
-    <Routes>
-      {
-        navbar.map((value)=>(
-          <Route path={value.path} element={value.element} />
-        ))
-      }
-    </Routes>
+    <></>
+    // <Routes>
+    //   {navbar.map(
+    //     (value) => {
+    //       console.log(value);
+    //     }
+    //     // <Route path={value.path} element={value.element} />
+    //   )}
+    // </Routes>
   );
 }
 
