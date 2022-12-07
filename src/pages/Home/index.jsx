2630ed87655dacc204f4298f8279bcb06dd142ec
https://github.com/Uraimoff <uraimovibrohim5@gmail.com>
2022-12-07 12:50:30 +0500

    home first

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import Timer from "../../components/Timer";
 import "./index.scss";
 const Home = () => {
+  const token = localStorage.getItem('token')
+  console.log(token, "bu token");
   const navigate = useNavigate()
   return (
     <div className="home-container ">
@@ -10,11 +12,20 @@ const Home = () => {
       <Timer />
       <div className="home-title GlobaWrapper">
         <h1>some information</h1>
-        <div className="authlog">
+    {
+      token ? 
+      <div style={{display: "none"}} className="authlog">
           <button onClick={()=>navigate('/register')}>Register</button>
           or
           <button onClick={()=>navigate('/login')}>Login</button>
         </div>
+: 
+      <div className="authlog">
+          <button onClick={()=>navigate('/register')}>Register</button>
+          or
+          <button onClick={()=>navigate('/login')}>Login</button>
+        </div>
+        }
       </div>
     </div>
   );
