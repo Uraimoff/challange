eacf958abcaec6150d1d0e699ca3dca5216ac2b6
https://github.com/Uraimoff <uraimovibrohim5@gmail.com>
2022-12-08 07:44:24 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,24 @@ import "./App.css";
 import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
 import Layout from "./components/Layout";
+import Admin from "./pages/Admin";
 
 function App() {
   const token = localStorage.getItem("token");
+  const email = localStorage.getItem("email");
+  const emaill = email.includes("qwerty1@com.uz") ? email.includes("qwerty1@com.uz") : false;
+  console.log(emaill, "bu email");
   return (
     <>
-      {token === null ? (
+      {token === null && emaill === null ? (
         <Routes>
           <Route path="/" element={<Navigate to={"/home"} />} />
           <Route path="*" element={<Navigate to={"/home"} />} />
-          <Route path="/home" element={<Home  />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/login" element={<SignIn />} />
         </Routes>
-      ) : (
+      ) : token !== null && emaill === null ? (
         <Layout>
           <Routes>
             <Route path="/" element={<Navigate to={"/home"} />} />
@@ -31,7 +35,18 @@ function App() {
             <Route path="/quest" element={<Question />} />
           </Routes>
         </Layout>
-      )}
+      ) : token !== null && emaill && emaill !== null ? (
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Navigate to={"/admin"} />} />
+            <Route path="*" element={<h1>404 Not found</h1>} />
+            <Route path="/admin" element={<Admin />} />
+            {/* <Route path="/all" element={<AllRate />} />
+            <Route path="/daily" element={<DailyRate />} />
+            <Route path="/quest" element={<Question />} /> */}
+          </Routes>
+        </Layout>
+      ) : null}
     </>
   );
 }
