b43ae399f4f8636f6dbc287d85a892d932fd52f0
https://github.com/Uraimoff <uraimovibrohim5@gmail.com>
2022-12-08 08:37:39 +0500

    admin page

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Tables from "../../components/Table";
+import Timer from "../../components/Timer";
+import "./style.scss";
 
 const Admin = () => {
   return (
-    <h1>Admin</h1>
-  )
-}
+    <>
+      <div className="allRw">
+        <div className="wrapper GlobaWrapper">
+          <div className="paddingTop"></div>
+          <div className="panel">
+            <div className="timewr">
+              <h2 className="time">
+                <h3 style={{ color: "black" }}>Question end time !</h3>
+                <Timer color={"black"} />
+              </h2>
+            </div>
+            <div className="paddingTop">
+              <Tables/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Admin
+export default Admin;
